56bb72c6894607aca6eefd17dad0db55d423b4a4
Sisay Chala <sisayie@gmail.com>
2026-02-07 23:19:42 +0100

    bug fixes

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "amharic-input-v2";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./transliterate.js",
+        "./app.js"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
